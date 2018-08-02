68c564562b39a32f3ee2ecd885f7a999b0f52bf8
Simon Daniel Bühlmann <sdbuehlmann@gmail.com>
2018-08-02 17:14:47 +0200

    Funktionierende uart kommunikation (senden blockierend, Empfangen interrupt gesteuert).

--- BoardSupport/Src/platform.c
+++ BoardSupport/Src/platform.c
@@ -13,7 +13,7 @@
 
 // =============== Defines =============================================================================================================
 #define MAX_PWM_DUTYCYCLE 1800
-#define UART_NR_BYTES 10
+#define UART_NR_BYTES 100
 
 // =============== Typdefs =============================================================================================================
 
@@ -21,10 +21,7 @@
 volatile uint8_t flag_hallA_ADC_isRunning = 0;
 volatile uint8_t flag_hallB_ADC_isRunning = 0;
 
-static uint8_t uartBufferA[UART_NR_BYTES];
-static uint8_t uartBufferB[UART_NR_BYTES];
-static uint8_t *pActiveUartBuffer;
-static uint8_t *pFirstFreeBufferSpace;
+uint8_t pBuffer[UART_NR_BYTES] = { 0 };
 
 // =============== Function pointers ===================================================================================================
 
@@ -55,7 +52,7 @@ void setPWMDutyCycle(uint8_t dutyCycle) {
 
 void enablePWM(Phase phase, Bridgeside side) {
 	switch (phase) {
-	case phase_A:
+		case phase_A:
 		if (side == bridgeside_highside) {
 			HAL_TIM_PWM_Start(pPWM_handle_A, PWM_A_HS_channel);
 		} else {
@@ -63,7 +60,7 @@ void enablePWM(Phase phase, Bridgeside side) {
 		}
 		break;
 
-	case phase_B:
+		case phase_B:
 		if (side == bridgeside_highside) {
 			HAL_TIM_PWM_Start(pPWM_handle_B_and_C, PWM_B_HS_channel);
 		} else {
@@ -71,7 +68,7 @@ void enablePWM(Phase phase, Bridgeside side) {
 		}
 		break;
 
-	case phase_C:
+		case phase_C:
 		if (side == bridgeside_highside) {
 			HAL_TIM_PWM_Start(pPWM_handle_B_and_C, PWM_C_HS_channel);
 		} else {
@@ -83,29 +80,29 @@ void enablePWM(Phase phase, Bridgeside side) {
 void disablePWM(Phase phase, Bridgeside side) {
 	switch (phase) {
 		case phase_A:
-			if (side == bridgeside_highside) {
-				HAL_TIM_PWM_Stop(pPWM_handle_A, PWM_A_HS_channel);
-			} else {
-				HAL_TIM_PWM_Stop(pPWM_handle_A, PWM_A_LS_channel);
-			}
-			break;
+		if (side == bridgeside_highside) {
+			HAL_TIM_PWM_Stop(pPWM_handle_A, PWM_A_HS_channel);
+		} else {
+			HAL_TIM_PWM_Stop(pPWM_handle_A, PWM_A_LS_channel);
+		}
+		break;
 
 		case phase_B:
-			if (side == bridgeside_highside) {
-				HAL_TIM_PWM_Stop(pPWM_handle_B_and_C, PWM_B_HS_channel);
-			} else {
-				HAL_TIM_PWM_Stop(pPWM_handle_B_and_C, PWM_B_LS_channel);
-			}
-			break;
+		if (side == bridgeside_highside) {
+			HAL_TIM_PWM_Stop(pPWM_handle_B_and_C, PWM_B_HS_channel);
+		} else {
+			HAL_TIM_PWM_Stop(pPWM_handle_B_and_C, PWM_B_LS_channel);
+		}
+		break;
 
 		case phase_C:
-			if (side == bridgeside_highside) {
-				HAL_TIM_PWM_Stop(pPWM_handle_B_and_C, PWM_C_HS_channel);
-			} else {
-				HAL_TIM_PWM_Stop(pPWM_handle_B_and_C, PWM_C_LS_channel);
-			}
-			break;
+		if (side == bridgeside_highside) {
+			HAL_TIM_PWM_Stop(pPWM_handle_B_and_C, PWM_C_HS_channel);
+		} else {
+			HAL_TIM_PWM_Stop(pPWM_handle_B_and_C, PWM_C_LS_channel);
 		}
+		break;
+	}
 }
 #endif /* PWM */
 
@@ -115,26 +112,26 @@ void enableComperator(Phase phase, Edge edge, Boolean enable) {
 	/*ToDo: edge berücksichtigen (umkonfigurieren)*/
 	if (enable == boolean_true) {
 		switch (phase) {
-		case phase_A:
+			case phase_A:
 			HAL_NVIC_EnableIRQ(IR_COMP_A_EXTI_IRQn);
 			break;
-		case phase_B:
+			case phase_B:
 			HAL_NVIC_EnableIRQ(IR_COMP_B_EXTI_IRQn);
 			break;
-		case phase_C:
+			case phase_C:
 			HAL_NVIC_EnableIRQ(IR_COMP_C_EXTI_IRQn);
 			break;
 		}
 
 	} else {
 		switch (phase) {
-		case phase_A:
+			case phase_A:
 			HAL_NVIC_DisableIRQ(IR_COMP_A_EXTI_IRQn);
 			break;
-		case phase_B:
+			case phase_B:
 			HAL_NVIC_DisableIRQ(IR_COMP_A_EXTI_IRQn);
 			break;
-		case phase_C:
+			case phase_C:
 			HAL_NVIC_DisableIRQ(IR_COMP_A_EXTI_IRQn);
 			break;
 		}
@@ -142,13 +139,13 @@ void enableComperator(Phase phase, Edge edge, Boolean enable) {
 }
 Boolean isComperatorSignalHigh(Phase phase) {
 	switch (phase) {
-	case phase_A:
+		case phase_A:
 		return HAL_GPIO_ReadPin(IR_COMP_A_GPIO_Port, IR_COMP_A_Pin);
-	case phase_B:
+		case phase_B:
 		return HAL_GPIO_ReadPin(IR_COMP_B_GPIO_Port, IR_COMP_B_Pin);
-	case phase_C:
+		case phase_C:
 		return HAL_GPIO_ReadPin(IR_COMP_C_GPIO_Port, IR_COMP_C_Pin);
-	default:
+		default:
 		platformError("Invalid phase.", __FILE__, __LINE__);
 		return 0;
 	}
@@ -178,7 +175,7 @@ Boolean isCalibrateEncoder() {
 ControlSignalType getControlSignalType() {
 	uint8_t temp = HAL_GPIO_ReadPin(DI_USER_IN_GPIO_Port, DI_USER_IN_Pin);
 
-	if(temp){
+	if (temp) {
 		return controlSignalType_positive_torque;
 	}
 	return controlSignalType_negative_torque;
@@ -196,8 +193,8 @@ void setDCCalBridgeDriver(Boolean dcCal) {
 
 // uart
 void sendUartData(uint8_t *pData, uint8_t size) {
-	HAL_UART_Transmit_IT(pUART_handle, pData, size);
-	//HAL_UART_Transmit(pUART_handle, &data, 1, 1);
+	//HAL_UART_Transmit_IT(pUART_handle, pData, size);
+	HAL_UART_Transmit(pUART_handle, pData, size, 10);
 }
 
 // encoder
@@ -235,22 +232,20 @@ void startupPlatform() {
 	HAL_TIM_Base_Start(pPWM_handle_A);
 	HAL_TIM_Base_Start(pPWM_handle_B_and_C);
 
-	// start receive over UART
-	pActiveUartBuffer = uartBufferA;
-	HAL_UART_Receive_IT(pUART_handle, pActiveUartBuffer, UART_NR_BYTES);
+	HAL_UART_Receive_IT(pUART_handle, pBuffer, UART_NR_BYTES);
 
 	startup();
 }
 void proceedPlatform() {
 	// handle UART
-	/*for(uint8_t cnt = 0; cnt < 100; cnt++){
-		uint8_t buffer = 0;
-		HAL_UART_Receive(pUART_handle, &buffer, 1, 1);
-
-		if(buffer != 0){
-			event_uartDataReceived(buffer);
+	for(uint8_t cnt = 0; cnt < UART_NR_BYTES; cnt++){
+		if(pBuffer[cnt] != 0){
+			//event_uartDataReceived(pBuffer[cnt]);
+			sendUartData(pBuffer + cnt, 1);
+			pBuffer[cnt] = 0;
 		}
-	}*/
+	}
+
 	proceed();
 }
 
@@ -291,10 +286,9 @@ void timerIRQ_encoderPulses() {
 }
 #endif /* ENCODER */
 
-void uartIRQ_dataSendet(){
+void uartIRQ_dataSendet() {
 
 }
-void uartIRQ_dataReceived(){
-
-	event_uartDataReceived(uartBufferA[0]);
+void uartIRQ_dataReceived() {
+	HAL_UART_Receive_IT(pUART_handle, pBuffer, UART_NR_BYTES); // restart
 }
--- Testing/Src/allTests.c
+++ Testing/Src/allTests.c
@@ -36,9 +36,12 @@ static uint8_t callback4Flag = 0;
 
 // =============== Functions ===========================================================================================================
 // --------------- platformAPI.h -------------------------------------------------------------------------------------------------------
-void assert(uint8_t isTrue){
+void assert(uint8_t isTrue, char file[], uint32_t line){
 	if(!isTrue){
-		// stop test and signal
+		uint8_t buffer[200] = { 0 };
+		sprintf(buffer, "\n\rTest failed! File: %s Line: %u\n\r", file, line);
+		sendUartData(buffer, 200);
+		// stop
 		while(1){
 			setLED(boolean_true);
 			wait_ms(200);
@@ -48,33 +51,25 @@ void assert(uint8_t isTrue){
 	}
 }
 
-void testSystime(){
-	uint32_t lastTimestamp_us = getSystimeUs();
-
-	wait_ms(MEDIUM_TIME_ms);
-
-	assert((lastTimestamp_us + MEDIUM_TIME_us) < getSystimeUs());
-}
-
 void callback1(){
 	callback1Flag = 1;
-	assert((timestamp_startDelayedCallbackTest + LONG_TIME_us) < getSystimeUs());
+	assert((timestamp_startDelayedCallbackTest + LONG_TIME_us) < getSystimeUs(), __FILE__, __LINE__);
 }
 void callback2(){
 	callback2Flag = 1;
-	assert((timestamp_startDelayedCallbackTest + MEDIUM_TIME_us) < getSystimeUs());
+	assert((timestamp_startDelayedCallbackTest + MEDIUM_TIME_us) < getSystimeUs(), __FILE__, __LINE__);
 }
 void callback3(){
 	callback3Flag = 1;
-	assert((timestamp_startDelayedCallbackTest + SHORT_TIME_us) < getSystimeUs());
+	assert((timestamp_startDelayedCallbackTest + SHORT_TIME_us) < getSystimeUs(), __FILE__, __LINE__);
 }
 void callback4(){
 	callback4Flag = 1;
-	assert((timestamp_startDelayedCallbackTest + ULTRA_SHORT_TIME_us) < getSystimeUs());
+	assert((timestamp_startDelayedCallbackTest + ULTRA_SHORT_TIME_us) < getSystimeUs(), __FILE__, __LINE__);
 }
 void callback5(){
 	// not to call
-	assert(0);
+	assert(0, __FILE__, __LINE__);
 }
 
 
@@ -87,48 +82,60 @@ void startTestDelayedCallback(){
 	startDelayedCallback(ULTRA_SHORT_TIME_us, &callback4);
 }
 
+void logString(char pMsg[]){
+	uint32_t cnt = 0;
+	uint8_t * pOrigMsg = pMsg;
+	while(*pMsg){
+		pMsg++;
+		cnt++;
+	}
+
+	sendUartData(pOrigMsg, cnt);
+}
+
 
 
 void startup(){
-	testSystime();
-	testSystime();
-	testSystime();
-
+	logString("\n\r*** START BLDC MOTOR DRIVER PLATFORM TESTS ***\n\r");
+	logString("Testing of time functions... ");
 	// ******** test first use *********************************************************************************
 	startTestDelayedCallback();
 	wait_ms(LONG_TIME_ms*2);
 	// check & reset flags
-	assert(callback1Flag);
+	assert(callback1Flag, __FILE__, __LINE__);
 	callback1Flag = 0;
-	assert(callback2Flag);
+	assert(callback2Flag, __FILE__, __LINE__);
 	callback2Flag = 0;
-	assert(callback3Flag);
+	assert(callback3Flag, __FILE__, __LINE__);
 	callback3Flag = 0;
-	assert(callback4Flag);
+	assert(callback4Flag, __FILE__, __LINE__);
 	callback4Flag = 0;
 
 	// ******** test second use *********************************************************************************
 	startTestDelayedCallback();
 	wait_ms(LONG_TIME_ms*2);
 	// check & reset flags
-	assert(callback1Flag);
+	assert(callback1Flag, __FILE__, __LINE__);
 	callback1Flag = 0;
-	assert(callback2Flag);
+	assert(callback2Flag, __FILE__, __LINE__);
 	callback2Flag = 0;
-	assert(callback3Flag);
+	assert(callback3Flag, __FILE__, __LINE__);
 	callback3Flag = 0;
-	assert(callback4Flag);
+	assert(callback4Flag, __FILE__, __LINE__);
 	callback4Flag = 0;
 
 	// ******** test platform error (not enough delayed callbacks) **********************************************
-	assert(!platformErrorFlag);
-	startTestDelayedCallback();
+	assert(!platformErrorFlag, __FILE__, __LINE__);
+	startDelayedCallback(LONG_TIME_us, &callback1);
+	startDelayedCallback(LONG_TIME_us, &callback2);
+	startDelayedCallback(LONG_TIME_us, &callback3);
+	startDelayedCallback(LONG_TIME_us, &callback4);
+	startDelayedCallback(LONG_TIME_us, &callback5);
 	// check flags
-	assert(platformErrorFlag);
-	assert(callback1Flag);
-	assert(callback2Flag);
-	assert(callback3Flag);
-	assert(callback4Flag);
+	assert(platformErrorFlag, __FILE__, __LINE__);
+	logString("Passed! \n\r");
+
+	logString("Passed all tests.\n\r");
 }
 void proceed(){
 	setLED(boolean_true); // passed all tests
